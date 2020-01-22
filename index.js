ff94db5712d7b1357ffc89ae0e5d05c72173f7b5
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-01-22 15:46:59 -0800

    completed MVP

--- index.js
+++ index.js
@@ -75,11 +75,13 @@ Person.prototype.toString = function() {
 function Car(model, milesPerGallon) {
   this.model = model;
   this.milesPerGallon = milesPerGallon;
-  this.tank = tank;
-  this.odometer = odometer;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
-Car.prototype.fill = function() {};
+Car.prototype.fill = function(gallons) {
+  return (this.tank = this.tank + gallons);
+};
 
 /*
   TASK 3
@@ -89,10 +91,15 @@ Car.prototype.fill = function() {};
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
-  this.name = name;
-  this.age = age;
+  Person.call(this, name, age);
   this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
